1e71332b7643dad71fb14e3f14df14c12d898594
Jack Archer <jacknimrodarcher@gmail.com>
2016-10-10 14:46:52 +1100

    Initial Commit

---
+++ ContactMySensors/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContactMySensors
+//
+//  Created by Jack N. Archer on 10/10/2016.
+//  Copyright © 2016 Jack N. Archer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
